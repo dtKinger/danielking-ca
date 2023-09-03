bf4caf1f766b81b15e3772ccc161fc32044a62d5
Dan King <king.daniel.thomas@gmail.com>
2023-09-03 20:48:36 +0700

    Clean up copy and unused js constants

--- scripts.js
+++ scripts.js
@@ -12,20 +12,17 @@ import { mobileNav } from "./nav-menu.js"
 // Init navigation
 mobileNav();
 
-const header = document.querySelector('header')
-const lightDarkToggle = document.querySelector('.theme-toggle')
-const heading1 = document.querySelector('h1')
-const buttons = document.querySelectorAll('button')
-const darkModeToggle = document.querySelector('.theme-toggle')
-const fixHeaderBtn = document.querySelector('.fixed-header-btn')
-const layoutContainer = document.querySelector('.layout-change')
-const profileContainer = document.querySelector('.profile')
-const crosshairContainer = document.querySelector('.crosshair-container')
+const header = document.querySelector('header');
+const lightDarkToggle = document.querySelector('.theme-toggle');
+const darkModeToggle = document.querySelector('.theme-toggle');
+const fixHeaderBtn = document.querySelector('.fixed-header-btn');
+const layoutContainer = document.querySelector('.layout-change');
 const backgroundSquares = document.querySelectorAll('.square');
-const foregroundSquares = document.querySelectorAll('.foreground-square');
-const previewItems = document.querySelectorAll('.preview-item')
-const hoverSpans = document.querySelectorAll('.hover-title');
+const previewItems = document.querySelectorAll('.preview-item');
 const ghostHeader = document.querySelector('.ghost-header');
+const githubCalendar = document.querySelector('.github-calendar');
+
+lightDarkToggle.addEventListener('click', toggleDarkMode)
 
 fixHeaderBtn.addEventListener('click', () => {
   header.classList.toggle('fixed-header');
@@ -35,25 +32,7 @@ fixHeaderBtn.addEventListener('click', () => {
 })
 
 
-lightDarkToggle.addEventListener('click', toggleDarkMode)
 
-function toggleDarkMode () {
-  if (darkModeStatus === false){
-    document.documentElement.style.setProperty('--text-color-default', 'white')
-    document.documentElement.style.setProperty('--bg-color-default', 'rgb(35, 35, 35)')
-    document.documentElement.style.setProperty('--accent-color-default', 'white')
-    document.documentElement.style.setProperty('--crosshair-border-color', 'white')
-    darkModeToggle.textContent = 'Light mode';
-  } else if (darkModeStatus === true){
-    document.documentElement.style.setProperty('--text-color-default', 'rgb(35, 35, 35)')
-    document.documentElement.style.setProperty('--bg-color-default', 'white')
-    document.documentElement.style.setProperty('--accent-color-default', 'rgb(35, 35, 35)')
-    document.documentElement.style.setProperty('--crosshair-border-color', 'rgb(35, 35, 35)')
-    darkModeToggle.textContent = 'Dark mode';
-  }
-  lightDarkToggle.classList.toggle('btn-setting-active');
-  darkModeStatus = !darkModeStatus; // toggle it.
-}
 
 backgroundSquares.forEach((square) => {
   square.addEventListener('mouseover', () => {
@@ -86,14 +65,40 @@ previewItems.forEach((item) => {
   })
 })
 
+githubCalendar.addEventListener('mouseover', () => {
+  githubCalendar.classList.toggle('show');
+})
 
 
-if (window.matchMedia) {
-  // check device prefers dark mode
-  const query = window.matchMedia('(prefers-color-scheme: dark)');
-  // if prefers: dark mode, run the click function
-  if (query.matches){
-    toggleDarkMode();
+
+// IIFE to set dark mode 
+let setDarkLightMode = ( () => {
+  if (window.matchMedia) {
+    // check device prefers dark mode
+    const query = window.matchMedia('(prefers-color-scheme: dark)');
+    // if prefers: dark mode, run the click function
+    if (query.matches){
+      toggleDarkMode();
+    }
   }
-}
+})()
 
+
+
+function toggleDarkMode () {
+  if (darkModeStatus === false){
+    document.documentElement.style.setProperty('--text-color-default', 'white')
+    document.documentElement.style.setProperty('--bg-color-default', 'rgb(35, 35, 35)')
+    document.documentElement.style.setProperty('--accent-color-default', 'white')
+    document.documentElement.style.setProperty('--crosshair-border-color', 'white')
+    darkModeToggle.textContent = 'Light mode';
+  } else if (darkModeStatus === true){
+    document.documentElement.style.setProperty('--text-color-default', 'rgb(35, 35, 35)')
+    document.documentElement.style.setProperty('--bg-color-default', 'white')
+    document.documentElement.style.setProperty('--accent-color-default', 'rgb(35, 35, 35)')
+    document.documentElement.style.setProperty('--crosshair-border-color', 'rgb(35, 35, 35)')
+    darkModeToggle.textContent = 'Dark mode';
+  }
+  lightDarkToggle.classList.toggle('btn-setting-active');
+  darkModeStatus = !darkModeStatus; // toggle it.
+}
